d4ef9d19b40dcb078f0d6e73d87aab0948de1d09
Ibrahimjamal98 <ibrahimjamal9812@gmail.com>
2023-09-26 18:24:23 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
+import { BsXCircleFill } from "react-icons/bs";
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+import "./App.css";
+import { questionsData } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [countTrue, setCountTrue] = useState(0);
+  const [countFalse, setCountFalse] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const getRandomIndex = () => {
+    return Math.floor(Math.random() * questionsData.length);
+  };
+  const changeQuestion = () => {
+    setCurrentQuestion(questionsData[getRandomIndex()]);
+  };
+  const handleClick = (answeredQuestion) => {
+    if (currentQuestion && currentQuestion.answear === answeredQuestion) {
+      setCountTrue(countTrue + 1);
+    } else {
+      setCountFalse(countFalse + 1);
+    }
+    changeQuestion();
+  };
+  useEffect(() => {
+    changeQuestion();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="app-container">
+      <h1>Matching Game Project</h1>
+      <div className="result">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <IoCheckmarkCircleOutline /> : {countTrue}
+        </p>
+        <p>
+          <BsXCircleFill /> : {countFalse}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {currentQuestion ? (
+        <div className="question-container">
+          <img
+            src={currentQuestion.imgSource}
+            alt={currentQuestion.question}
+            className="image-container"
+          />
+          <p>{currentQuestion.question}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <div className="card">
+        <button className="yes-button" onClick={() => handleClick(true)}>
+          <AiOutlineCheck />
+        </button>
+        <button className="no-button" onClick={() => handleClick(false)}>
+          <AiOutlineClose />
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
---
+++ src/data.js
@@ -0,0 +1,68 @@
+export const questionsData = [
+  {
+    imgSource:
+      "https://img.taste.com.au/lNnNoTvU/taste/2010/01/sushi-187034-1.jpg",
+    question: "this is pizza",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://static01.nyt.com/images/2022/02/09/multimedia/09starbucks-union/09starbucks-union-superJumbo.jpg",
+    question: "this is Aroma Cafe",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://www.hindustantimes.com/ht-img/img/2023/08/25/1600x900/international_dog_day_1692974397743_1692974414085.jpg",
+    question: "this is dog",
+    answear: true,
+  },
+  {
+    imgSource:
+      "https://ocean.si.edu/sites/default/files/styles/full_width_largest/public/25691336465_982637ea03_o.jpg.webp?itok=i3ExPDcs",
+    question: "this is river",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Left_side_of_Flying_Pigeon.jpg/1200px-Left_side_of_Flying_Pigeon.jpg",
+    question: "this is car",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://static.javatpoint.com/computer/images/how-to-use-a-computer-keyboard.png",
+    question: "this is keyboard",
+    answear: true,
+  },
+  {
+    imgSource:
+      "https://www.woodlandtrust.org.uk/media/50820/house-mouse-wtml-1062551-amy-lewis.jpg",
+    question: "this is mouse",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Nh-pluto-in-true-color_2x.jpg/800px-Nh-pluto-in-true-color_2x.jpg",
+    question: "this is Earth",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7c1763bd/images/2010000/2014279.jpg?sfrm=jpg",
+    question: "this is table",
+    answear: true,
+  },
+  {
+    imgSource:
+      "https://www.travelandleisure.com/thmb/SPUPzO88ZXq6P4Sm4mC5Xuinoik=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg",
+    question: "this is berlin",
+    answear: false,
+  },
+  {
+    imgSource:
+      "https://www.travelandleisure.com/thmb/WZfCPml-1I4pC4JFeYdTsp5BYQw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/toronto-ontario-canada-lead-TORONTOTG0721-bfa723dac6c741d0bd549c90c2660968.jpg",
+    question: "this is Toronto",
+    answear: true,
+  },
+];
